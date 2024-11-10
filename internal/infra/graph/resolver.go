774a0de6ef65f7eaf6db1ba04ce9243bfee75550
Rodolfo Quenallata <rodolfolucas12@gmail.com>
2024-11-10 18:44:15 -0300

    adding create order

---
+++ internal/infra/graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/rodolfolucas12/clean-archit/internal/usecase"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	CreateOrderUseCase usecase.CreateOrderUseCase
+}
