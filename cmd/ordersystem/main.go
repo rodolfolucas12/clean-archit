ab1d01b37755184b64d22e0e32adb08d37e5a698
Rodolfo Quenallata <rodolfolucas12@gmail.com>
2024-11-10 19:37:23 -0300

    adding list orders to the web server

--- cmd/ordersystem/main.go
+++ cmd/ordersystem/main.go
@@ -9,11 +9,15 @@ import (
 	graphql_handler "github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/rodolfolucas12/clean-archit/configs"
+	"github.com/rodolfolucas12/clean-archit/internal/event"
 	"github.com/rodolfolucas12/clean-archit/internal/event/handler"
+	"github.com/rodolfolucas12/clean-archit/internal/infra/database"
 	"github.com/rodolfolucas12/clean-archit/internal/infra/graph"
 	"github.com/rodolfolucas12/clean-archit/internal/infra/grpc/pb"
 	"github.com/rodolfolucas12/clean-archit/internal/infra/grpc/service"
+	"github.com/rodolfolucas12/clean-archit/internal/infra/web"
 	"github.com/rodolfolucas12/clean-archit/internal/infra/web/webserver"
+	"github.com/rodolfolucas12/clean-archit/internal/usecase"
 	"github.com/rodolfolucas12/clean-archit/pkg/events"
 	"github.com/streadway/amqp"
 	"google.golang.org/grpc"
@@ -42,11 +46,19 @@ func main() {
 		RabbitMQChannel: rabbitMQChannel,
 	})
 
-	createOrderUseCase := NewCreateOrderUseCase(db, eventDispatcher)
+	orderRepository := database.NewOrderRepository(db)
+	err = orderRepository.CreateTable()
+	if err != nil {
+		panic(err)
+	}
+
+	orderCreated := event.NewOrderCreated()
+	webOrderHandler := web.NewWebOrderHandler(eventDispatcher, orderRepository, orderCreated)
+	createOrderUseCase := usecase.NewCreateOrderUseCase(orderRepository, orderCreated, eventDispatcher)
 
 	webserver := webserver.NewWebServer(configs.WebServerPort)
-	webOrderHandler := NewWebOrderHandler(db, eventDispatcher)
-	webserver.AddHandler("/order", webOrderHandler.Create)
+	webserver.AddHandler("POST", "/order", webOrderHandler.Create)
+	webserver.AddHandler("GET", "/order", webOrderHandler.ListOrders)
 	fmt.Println("Starting web server on port", configs.WebServerPort)
 	go webserver.Start()
 
